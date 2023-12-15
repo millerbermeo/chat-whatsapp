c39764040fbe11ff31ecdc78526697c434226d20
millerbermeo <millerbermeo30@gmail.com>
2023-12-15 08:17:22 -0500

    commit

--- src/components/ChatMenssage.jsx
+++ src/components/ChatMenssage.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import data from '@emoji-mart/data';
 import Picker from '@emoji-mart/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser,faDownload,faFile , faUserTie , faCloudArrowUp, faIcons, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faDownload, faFile, faUserTie, faCloudArrowUp, faIcons, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 function ChatMenssage({ numeroSeleccionado }) {
   const [mensajes, setMensajes] = useState([]);
@@ -15,6 +15,8 @@ function ChatMenssage({ numeroSeleccionado }) {
   const lastMessageRef = useRef(null);
   const [shouldScrollToLast, setShouldScrollToLast] = useState(true);
   const [loading, setLoading] = useState(false);
+  const [isFileUploaded, setIsFileUploaded] = useState(false); // Nuevo estado
+
 
 
 
@@ -85,9 +87,11 @@ function ChatMenssage({ numeroSeleccionado }) {
 
         // Update loading state while uploading the document
         setLoading(true);
+        setIsFileUploaded(true);
       } else {
         // If it's not a document, reset the loading state
         setLoading(false);
+
       }
 
       mensajeInputRef.current.value = '';
@@ -105,6 +109,7 @@ function ChatMenssage({ numeroSeleccionado }) {
     } finally {
       // Clear loading state after the upload is complete or in case of an error
       setLoading(false);
+      setIsFileUploaded(false);
 
       // Restablecer valores
       // También puedes restablecer el estado de otros elementos si es necesario
@@ -146,7 +151,9 @@ function ChatMenssage({ numeroSeleccionado }) {
   };
 
 
-
+  const handleFileInputChange = () => {
+    setIsFileUploaded(archivoInputRef.current.files.length > 0);
+  };
 
   const handleKeyDown = (e) => {
     if (e.key === 'Enter' && !e.shiftKey) {
@@ -220,7 +227,7 @@ function ChatMenssage({ numeroSeleccionado }) {
           <div className="absolute bottom-2" style={mensaje.b1 === '1' ? { right: '-10px' } : { left: '-10px' }}>
             <a target='_blank' href={mensaje.url} download>
               <button className="bg-[#005187] hover:bg-[#005187]/80 text-white font-bold px-2 py-1 rounded">
-              <FontAwesomeIcon icon={faDownload} />
+                <FontAwesomeIcon icon={faDownload} />
               </button>
             </a>
           </div>
@@ -234,7 +241,7 @@ function ChatMenssage({ numeroSeleccionado }) {
               {mensaje.men}
             </span>
             <span className='absolute top-0' style={mensaje.b1 === '1' ? { right: '-20px' } : { left: '-20px' }}>
-            <FontAwesomeIcon icon={faFile} className="text-3xl text-white" />
+              <FontAwesomeIcon icon={faFile} className="text-3xl text-white" />
             </span>
           </button>
         </a>
@@ -291,96 +298,97 @@ function ChatMenssage({ numeroSeleccionado }) {
 
   return (
     <>
-    <div className='w-full md:w-[95%] h-[87vh] md:h-[95vh] shadow-lg relative z-auto flex flex-col bg-gray-200'>
-      {/* ... (resto del código) */}
-      <div className="w-full mt-5 lg:mt-14 pb-[15px] h-[100%] overflow-y-scroll custom-scrollbar3 px-4 md:px-12 bg-gray-100" ref={(ref) => setScrollRef(ref)}>
-        <div className='absolute bottom-16 left-[40px] flex items-center flex-col z-50'>
-          {mostrarDiv ?
-            <Picker data={data} onEmojiSelect={handleEmojiClick} /> : ''}
-        </div>
-        <ul className="">
-          {mensajes.map((mensaje, index) => (
-            <li
-              key={index}
-              className={`flex items-end justify-${mensaje.b1 === '2' ? 'end' : 'start'} py-2 gap-2`}
-              ref={index === mensajes.length - 1 ? lastMessageRef : null}
-            >
-              {mensaje.b1 === '2' ? (
-                <>
-                  <div className="text-black break-all text-[15px] shadow bg-[#84b6f4] flex-wrap flex max-w-[65%] rounded-lg p-[7px] pl-10 text-left">
-                    {renderMedia(mensaje)}
-                  </div>
-                  <div className='border border-[#84b6f4] text-2xl w-10 h-10 grid place-items-center text-[#84b6f4] bg-gray-200 rounded-full'>
-                  <FontAwesomeIcon icon={faUserTie} className="" />
-                  </div>
-
-                </>
-              ) : (
-                <>
-                <div className='border border-gray-300 text-2xl w-10 h-10 grid place-items-center text-gray-400 bg-gray-200 rounded-full'>
-                <FontAwesomeIcon icon={faUser} className="" />
-                </div>
-                  <div className="text-black text-[15px] break-all shadow bg-gray-300 flex-wrap flex max-w-[65%]  rounded-lg p-[7px] pr-10">{renderMedia(mensaje)}</div>
-                </>
-              )}
-            </li>
-          ))}
-          {fullscreenImage && (
-            <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-90  z-50 flex items-center justify-center" onClick={() => setFullscreenImage(null)}>
-              <img
-                src={fullscreenImage}
-                alt="Imagen a pantalla completa"
-                className="w-[40%] h-auto max-h-[90%]"
-              />
-            </div>
-          )}
-        </ul>
-        {loading && (
-          <div className="text-red-600 absolute bottom-20 right-72">Cargando...</div>
-        )}
-      </div>
-      <div className='w-full flex items-center justify-center h-14 bg-gray-200 bottom-0'>
-        <div className="w-[90%] mx-auto p-2 gap-2 flex">
-          <button onClick={handleReloadPage} className='md:hidden' type='submit'>
-            <div className='w-[40px] h-[40px] bg-[#000] rounded-[25px] text-white flex justify-center items-center text-2xl'>
-              <FontAwesomeIcon icon={faChevronLeft} />
-            </div>
-          </button>
-          <div className="relative w-full text-gray-600">
-            <div className='ml-10 absolute -left-8 top-2'>
-              <input
-                ref={archivoInputRef}
-                id="fileInput"
-                type="file"
-                accept=".pdf, .jpg, .jpeg, .png, .gif, .mp4, .webm, .ogg"
-                className="hidden"
-              />
-              <label htmlFor="fileInput" className="file-input-label">
-                <FontAwesomeIcon icon={faCloudArrowUp} />
-              </label>
-            </div>
-            <div className='ml-10 absolute -left-2 top-2'>
-              <button onClick={toggleDiv}>
-                <FontAwesomeIcon icon={faIcons} />
-              </button>
-            </div>
-            <input
-              ref={mensajeInputRef}
-              onKeyDown={handleKeyDown}
-              className="w-full border-2 border-gray-300 bg-white h-10 px-8 pl-14 pr-16 rounded-lg text-sm focus:outline-none focus:border-blue-500"
-              type="text"
-              placeholder="Escribe algo..."
-            />
+      <div className='w-full md:w-[95%] h-[87vh] md:h-[95vh] shadow-lg relative z-auto flex flex-col bg-gray-200'>
+        {/* ... (resto del código) */}
+        <div className="w-full mt-5 lg:mt-14 pb-[15px] h-[100%] overflow-y-scroll custom-scrollbar3 px-4 md:px-12 bg-gray-100" ref={(ref) => setScrollRef(ref)}>
+          <div className='absolute bottom-16 left-[40px] flex items-center flex-col z-50'>
+            {mostrarDiv ?
+              <Picker data={data} onEmojiSelect={handleEmojiClick} /> : ''}
           </div>
-          <button type='submit' onClick={enviarMensaje}>
-            <div className='w-[35px] h-[35px] bg-green-500 rounded-[25px] text-white flex justify-center items-center text-2xl'>
-              <FontAwesomeIcon icon={faChevronRight} />
+          <ul className="">
+            {mensajes.map((mensaje, index) => (
+              <li
+                key={index}
+                className={`flex items-end justify-${mensaje.b1 === '2' ? 'end' : 'start'} py-2 gap-2`}
+                ref={index === mensajes.length - 1 ? lastMessageRef : null}
+              >
+                {mensaje.b1 === '2' ? (
+                  <>
+                    <div className="text-black break-all text-[15px] shadow bg-[#84b6f4] flex-wrap flex max-w-[65%] rounded-lg p-[7px] pl-10 text-left">
+                      {renderMedia(mensaje)}
+                    </div>
+                    <div className='border border-[#84b6f4] text-2xl w-10 h-10 grid place-items-center text-[#84b6f4] bg-gray-200 rounded-full'>
+                      <FontAwesomeIcon icon={faUserTie} className="" />
+                    </div>
+
+                  </>
+                ) : (
+                  <>
+                    <div className='border border-gray-300 text-2xl w-10 h-10 grid place-items-center text-gray-400 bg-gray-200 rounded-full'>
+                      <FontAwesomeIcon icon={faUser} className="" />
+                    </div>
+                    <div className="text-black text-[15px] break-all shadow bg-gray-300 flex-wrap flex max-w-[65%]  rounded-lg p-[7px] pr-10">{renderMedia(mensaje)}</div>
+                  </>
+                )}
+              </li>
+            ))}
+            {fullscreenImage && (
+              <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-90  z-50 flex items-center justify-center" onClick={() => setFullscreenImage(null)}>
+                <img
+                  src={fullscreenImage}
+                  alt="Imagen a pantalla completa"
+                  className="w-[40%] h-auto max-h-[90%]"
+                />
+              </div>
+            )}
+          </ul>
+          {loading && (
+            <div className="text-red-600 absolute bottom-20 right-72">Cargando...</div>
+          )}
+        </div>
+        <div className='w-full flex items-center justify-center h-14 bg-gray-200 bottom-0'>
+          <div className="w-[90%] mx-auto p-2 gap-2 flex">
+            <button onClick={handleReloadPage} className='md:hidden' type='submit'>
+              <div className='w-[40px] h-[40px] bg-[#000] rounded-[25px] text-white flex justify-center items-center text-2xl'>
+                <FontAwesomeIcon icon={faChevronLeft} />
+              </div>
+            </button>
+            <div className="relative w-full text-gray-600">
+              <div className='ml-10 absolute -left-8 top-2'>
+                <input
+                  ref={archivoInputRef}
+                  id="fileInput"
+                  type="file"
+                  accept=".pdf, .jpg, .jpeg, .png, .gif, .mp4, .webm, .ogg"
+                  className="hidden"
+                  onChange={handleFileInputChange}
+                />
+                <label htmlFor="fileInput" className={`file-input-label ${isFileUploaded ? 'file-uploaded' : ''}`}>
+                  <FontAwesomeIcon icon={faCloudArrowUp} />
+                </label>
+              </div>
+              <div className='ml-10 absolute -left-2 top-2'>
+                <button onClick={toggleDiv}>
+                  <FontAwesomeIcon icon={faIcons} />
+                </button>
+              </div>
+              <input
+                ref={mensajeInputRef}
+                onKeyDown={handleKeyDown}
+                className="w-full border-2 border-gray-300 bg-white h-10 px-8 pl-14 pr-16 rounded-lg text-sm focus:outline-none focus:border-blue-500"
+                type="text"
+                placeholder="Escribe algo..."
+              />
             </div>
-          </button>
+            <button type='submit' onClick={enviarMensaje}>
+              <div className='w-[35px] h-[35px] bg-green-500 rounded-[25px] text-white flex justify-center items-center text-2xl'>
+                <FontAwesomeIcon icon={faChevronRight} />
+              </div>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  </>
+    </>
   );
 }
 
